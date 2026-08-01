0df356383fbdd4c46a31e738d77bf628ce8c0a66
hexpertify-intern-005 <hexpertifyintern005@outlook.com>
2026-08-01 18:08:31 +0530

    Resolved merge conflicts

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
 export default {
-<<<<<<< HEAD
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
-=======
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
-}
->>>>>>> 441331a878018850586e807c35562f0331c2dc7b
--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import seoReducer from './seoSlice';
-
-export const store = configureStore({
-    reducer: {
-        seo: seoReducer,
-    },
-});
